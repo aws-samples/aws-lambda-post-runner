2a306660657bad64d7a63acaf1de24f8ca17c7dd
DiscreteTom <discrete_tom@outlook.com>
2025-09-09 08:41:06 +0000

    chore: better error handling for the entry.sh

--- scripts/entry.sh
+++ scripts/entry.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+if [[ ! -x "/opt/aws-lambda-post-runner" ]]; then
+    echo "Error: /opt/aws-lambda-post-runner not found or not executable" >&2
+    exit 1
+fi
+
 exec /opt/aws-lambda-post-runner "$@"
